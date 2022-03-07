adadeffe2befe633ce7e9d72cffc3aa5d11f6171
natsumeshow <42426886+natsumeshow@users.noreply.github.com>
2022-03-07 16:50:51 +0900

    Update tracker.py

--- deep_sort_pytorch/deep_sort/sort/tracker.py
+++ deep_sort_pytorch/deep_sort/sort/tracker.py
@@ -1,7 +1,8 @@
 # vim: expandtab:ts=4:sw=4
 from __future__ import absolute_import
 import numpy as np
-from . import kalman_filter
+#from . import kalman_filter
+from . import extended_kalman_filter
 from . import linear_assignment
 from . import iou_matching
 from .track import Track
